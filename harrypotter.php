a56874cbbbfbfa6c8970c0e86243949c7542c172
chem.chikweze <53623147+chem-chikweze@users.noreply.github.com>
2023-12-18 22:38:21 -0500

    banner complete

---
+++ harrypotter.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1 - Harry Potter</title>
+    <!-- this is just for the table to looks right on the webpage -->
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- HEADER section -->
+    <header>
+        <h1>Harry Potter and its magic world</h1>
+         <!-- this image will be resized and format later -->
+        <img src="images/hogwarts.jpg" alt="hogwarts castle">
+    </header>
+
+    <!-- MAIN content -->
+    <main>
+        <!-- SECTION for Brief introduction -->
+        <section>
+            <h2>Introduction</h2>
+            <p>Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. The main story arc concerns Harry's conflict with Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic, and subjugate all wizards and Muggles (non-magical people).</p>
+            <p>The series was originally published in English by Bloomsbury in the United Kingdom and Scholastic Press in the United States. A series of many genres, including fantasy, drama, coming-of-age fiction, and the British school story (which includes elements of mystery, thriller, adventure, horror, and romance), the world of Harry Potter explores numerous themes and includes many cultural meanings and references.[1] According to Rowling, the main theme is death.[2] Other major themes in the series include prejudice, corruption, and madness.</p>
+            <p>Since the release of the first novel, Harry Potter and the Philosopher's Stone, on 26 June 1997, the books have found immense popularity, positive reviews, and commercial success worldwide. They have attracted a wide adult audience as well as younger readers, and are widely considered cornerstones of modern literature.[4][5] As of February 2023, the books have sold more than 600 million copies worldwide, making them the best-selling book series in history, and have been available in 85 languages.[6] The last four books consecutively set records as the fastest-selling books in history, with the final instalment selling roughly 2.7 million copies in the United Kingdom and 8.3 million copies in the United States within twenty-four hours of its release.</p>
+            <p>The original seven books were adapted into an eight-part namesake film series by Warner Bros. Pictures. In 2016, the total value of the Harry Potter franchise was estimated at $25 billion,[7] making Harry Potter one of the highest-grossing media franchises of all time. Harry Potter and the Cursed Child is a play based on a story co-written by Rowling.
+            The success of the books and films has allowed the Harry Potter franchise to expand with numerous derivative works, a travelling exhibition that premiered in Chicago in 2009, a studio tour in London that opened in 2012, a digital platform on which J. K. Rowling updates the series with new information and insight, and a pentalogy of spin-off films premiering in November 2016 with Fantastic Beasts and Where to Find Them, among many other developments. Themed attractions, collectively known as The Wizarding World of Harry Potter, have been built at several Universal Destinations & Experiences amusement parks around the world.</p>
+        </section>
+
+        <article>
+            <h2>Plot</h2>
+            <h3>Early years</h3>
+            <p>The series follows the life of a boy named Harry Potter. In the first book, Harry Potter and the Philosopher's Stone, Harry lives in a cupboard under the stairs in the house of the Dursleys, his aunt, uncle and cousin, who all treat him poorly. At the age of 11, Harry discovers that he is a wizard. He meets a half-giant named Hagrid who invites him to attend the Hogwarts School of Witchcraft and Wizardry. Harry learns that his parents, Lily and James Potter, were also magicians, and were murdered by the dark wizard Lord Voldemort when Harry was a baby. When Voldemort attempted to kill Harry, his curse rebounded, seemingly killing Voldemort, and Harry survived with a lightning-shaped scar on his forehead. The event made Harry famous among the community of wizards and witches.</p>
+             <p>Harry becomes a student at Hogwarts and is sorted into Gryffindor House. He gains the friendship of Ron Weasley, a member of a large but poor wizarding family, and Hermione Granger, a witch of non-magical, or Muggle, parentage. The trio develop an enmity with the rich pure-blood student Draco Malfoy. Harry encounters the school's headmaster, Albus Dumbledore; the potions professor, Severus Snape, who displays a dislike for him; and the Defence Against the Dark Arts teacher, Quirinus Quirrell. Quirrell turns out to be allied with Voldemort, who is still alive as a weak spirit. The first book concludes with Harry's confrontation with Voldemort, who, in his quest to regain a body, yearns to possess the Philosopher's Stone, a substance that bestows everlasting life.
+            </p>
+            <p>In the third novel, Harry Potter and the Prisoner of Azkaban, Harry learns that he is targeted by Sirius Black, an escaped convict who allegedly assisted in his parents' murder. Dementors, creatures that feed on despair, search for Sirius and guards the school. As Harry struggles with his reaction to the dementors, he reaches out to Remus Lupin, a new professor who teaches him the Patronus charm. On a windy night, Ron is dragged by a black dog into the Shrieking Shack, a haunted house, and Harry and Hermione follow. The dog is revealed to be Sirius Black. Lupin enters the shack and explains that Sirius was James Potter's best friend; he was framed by another friend of James, Peter Pettigrew, who hides as Ron's pet rat, Scabbers. As the full moon rises, Lupin transforms into a werewolf and bounds away, and the group chase after him. They are surrounded by dementors, but are saved by a figure resembling James who casts a stag Patronus. This is later revealed to be a future version of Harry, who traveled back in time with Hermione using a device called a Time Turner. The duo help Sirius escape on a Hippogriff, while Pettigrew escapes.</p>
+
+            <h3>Voldemort returns</h3>
+            <p>In Harry's fourth year of school (detailed in Harry Potter and the Goblet of Fire), Hogwarts hosts the Triwizard Tournament, a contest between Hogwarts and the schools Beauxbatons and Durmstrang. Harry is unwillingly entered into the contest, becoming Hogwarts' second participant after Cedric Diggory, an unusual occurrence that causes his friends to distance themselves from him. Harry claims the Triwizard Cup with Cedric, but in doing so is teleported to a graveyard where Pettigrew kills Cedric, then resurrects Voldemort using Harry's blood. Voldemort convenes his supporters, the Death Eaters, and Harry manages to escape after a duel with Voldemort. Upon returning to Hogwarts, it is revealed that a Death Eater, Barty Crouch, Jr, in disguise as the new Defence Against the Dark Arts professor, Alastor "Mad-Eye" Moody, engineered Harry's entry into the tournament, secretly helped him, and had him teleported to Voldemort.
+            </p>
+            <p>In the fifth book, Harry Potter and the Order of the Phoenix, the Ministry of Magic refuses to believe that Voldemort has returned. Dumbledore re-activates the Order of the Phoenix, a secret society to counter Voldemort; meanwhile, the Ministry tightens control of Hogwarts by appointing Dolores Umbridge as High Inquisitor of Hogwarts, and she gradually increases her powers. When Umbridge bans practical teaching of Defence Against the Dark Arts, Harry, Ron and Hermione form "Dumbledore's Army", a secret group to continue the teachings. Harry has recurring dreams of a dark corridor in the Ministry of Magic, eventually dreaming that Sirius is being tortured there. He races to the Ministry with his friends, but it is a trap, planted in his head by Voldemort. The group are attacked by Death Eaters and saved by the Order of the Phoenix, but Sirius is killed in the battle. A duel between Dumbledore and Voldemort convinces the ministry of Voldemort's return. A prophecy concerning Harry and Voldemort is revealed: one must die at the hands of the other.</p>
+            <p>In Harry Potter and the Deathly Hallows, the seventh and final novel in the series, Lord Voldemort gains control of the Ministry of Magic. Harry, Ron and Hermione learn about the Deathly Hallows, legendary items that lead to mastery over death. The group infiltrate the ministry, where they steal a locket Horcrux, and visit Godric's Hollow, where they are attacked by Nagini, Voldemort's snake. A silver doe Patronus leads them to the Sword of Gryffindor, with which they destroy the locket. They steal a Horcrux from Gringotts and travel to Hogwarts, culminating in a battle with Death Eaters. Snape is killed by Voldemort out of paranoia, but lends Harry his memories before he dies. Harry learns that Snape was always loyal to Dumbledore, and that Harry is himself a Horcrux. Harry surrenders to Voldemort and dies. The defenders of Hogwarts continue to fight on; Harry is resurrected, faces Voldemort and kills him.</p>
+        </article>
+
+        <!-- ASIDE table for Wands -->
+        <aside>
+            <h2>Wand Woods</h2>
+            <div><em>Most wands were long, thin rods of wood with a magical substance embedded into their core. Among the woods used were:</em></div>
+
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Wood</th>
+                        <th>Genus; binomial name</th>
+                        <th>Garrick Ollivander's notes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Acacia</td>
+                        <td>Acacia</td>
+                        <td>A very unusual wand wood, which I have found creates tricky wands that often refuse to produce magic for any but their owner, and also withhold their best effects from all but those most gifted.</td>
+                    </tr>
+                    <tr>
+                        <td>Alder</td>
+                        <td>Alnus</td>
+                        <td>Alder is an unyielding wood, yet I have discovered that its ideal owner is not stubborn or obstinate, but often helpful, considerate and most likeable. Alder wood is well suited for making flutes and pipes, and for building bridges. Alder people are adventurous travellers and confident decision makers who trust their inner voices. Alder people should work hard to maintain a balance between work and play.[16]</td>
+                    </tr>
+                    <tr>
+                        <td>Apple</td>
+                        <td>Malus Domestic</td>
+                        <td>Applewood wands are not made in great numbers. They are powerful and best suited to an owner of high aims and ideals, as this wood mixes poorly with Dark magic.</td>
+                    </tr>
+                    <tr>
+                        <td>Ash</td>
+                        <td>Fraxinus</td>
+                        <td>The ash wand cleaves to its one true master and ought not to be passed on or gifted from the original owner, because it will lose power and skill. This tendency is extreme if the core is of unicorn. Tools, magical and ordinary, made from Ash are especially productive as Ash trees are known to attract energy. Ash people are kind and generous with a gift for seeing what is beautiful in the world and in others. Ash people should be careful that their romantic hearts do not lead them into danger.[17]</td>
+                    </tr>
+                    <tr>
+                        <td>Cedar</td>
+                        <td>Cedrus</td>
+                        <td>Whenever I meet one who carries a cedar wand, I find strength of character and unusual loyalty. My father, Gervaise Ollivander, used always to say, 'you will never fool the cedar carrier,' and I agree: the cedar wand finds its perfect home where there is perspicacity and perception.</td>
+                    </tr>
+                    <tr>
+                        <td>Pine</td>
+                        <td>Pinus</td>
+                        <td>The straight-grained pine wand always chooses an independent, individual master who may be perceived as a loner, intriguing and perhaps mysterious. Pine wands enjoy being used creatively, and unlike some others, will adapt unprotestingly to new methods and spells.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </aside>
+
+        <!-- ASIDE list for Wands -->
+        <aside>
+            <h2>Lists of Wand Cores</h2>
+            <div><em>Most wands made by Garrick Ollivander contained one of the three supreme core types, but there were others.Among the substances used were:</em></div>
+            <ul>
+                <li>Unicorn hair</li>
+                <li>Dragon heartstring</li>
+                <li>Phoenix feather</li>
+                <li>Veela hair</li>
+                <li>Thestral tail</li>
+                <li>Thunderbird tail feather</li>
+            </ul>
+            <img src="images/wand.jpg" alt="picture of wands">
+        </aside>
+
+        <section>
+            <h2>Themes</h2>
+            <p>Harry Potter's overarching theme is death. In the first book, when Harry looks into the Mirror of Erised, he feels both joy and "a terrible sadness" at seeing his desire: his parents, alive and with him. Confronting their loss is central to Harry's character arc and manifests in different ways through the series, such as in his struggles with Dementors. Other characters in Harry's life die; he even faces his own death in Harry Potter and the Deathly Hallows. The series has an existential perspective – Harry must grow mature enough to accept death. In Harry's world, death is not binary but mutable, a state that exists in degrees. Unlike Voldemort, who evades death by separating and hiding his soul in seven parts, Harry's soul is whole, nourished by friendship and love.</p>
+            <p><strong><em>Love distinguishes Harry and Voldemort. Harry is a hero because he loves others, even willing to accept death to save them; Voldemort is a villain because he does not. Harry carries the protection of his mother's sacrifice in his blood; Voldemort, who wants Harry's blood and the protection it carries, does not understand that love vanquishes death.</em></strong></p>
+            <p>While Harry Potter can be viewed as a story about good vs. evil, its moral divisions are not absolute. First impressions of characters are often misleading. Harry assumes in the first book that Quirrell is on the side of good because he opposes Snape, who appears to be malicious; in reality, Quirrell is an agent of Voldemort, while Snape is loyal to Dumbledore. This pattern later recurs with Moody and Snape. In Rowling's world, good and evil are choices rather than inherent attributes: second chances and the possibility of redemption are key themes of the series. This is reflected in Harry's self-doubts after learning his connections to Voldemort, such as Parseltongue; and prominently in Snape's characterisation, which has been described as complex and multifaceted. In some scholars' view, while Rowling's narrative appears on the surface to be about Harry, her focus may actually be on Snape's morality and character arc.</p>
+        </section>
+
+        <section>
+            <h2>Loved Characters</h2>
+            <div> The "Harry Potter" series boasts a large number of characters, ranging from the primary protagonists to various supporting and minor characters. Here's a list of some of the most notable characters from the series:</div>
+           <ul>
+                <li>Harry Potter</li>
+                <li>Hermione Granger</li>
+                <li>Ron Weasley</li>
+                <li>Fred Weasley</li>
+                <li>George Weasley</li>
+                <li>Albus Dumbledore</li>
+                <li>Severus Snape</li>
+                <li>Minerva McGonagall</li>
+                <li>Rubeus Hagrid</li>
+                <li>Neville Longbottom</li>
+                <li>Luna Lovegood</li>
+                <li>Cho Chang</li>
+                <li>Cedric Diggory</li>
+                <li>Draco Malfoy</li>
+            </ul>
+        </section>
+
+        <!-- ARTICLE on Cedric Diggory's Life -->
+        <article>
+            <h2>One of my favorite character: Cedric Diggory</h2>
+            <div><em><strong>While the series primarily focuses on Harry, the expansive universe Rowling built is filled with numerous intriguing characters. One such character, who made a significant impact despite his brief appearance, is Cedric Diggory.</em></strong></div>
+            <img src="images/ceddd.jpg" alt="Description of image 2">
+
+            <h3>Cedric Diggory's Early Life</h3>
+            <p>Cedric Diggory hailed from a wizarding family known for its honorable lineage. The Diggorys, though not as famous as the Potters or the Weasleys, had a legacy at Hogwarts, primarily associated with the House of Hufflepuff. A tall young man with a striking presence, Cedric's deep-set gray eyes and wavy hair made him stand out in a crowd. Attending Hogwarts, he was sorted into Hufflepuff. It's during his time here that Cedric showcased qualities like fairness, bravery, and determination, which endeared him to many, including Professor Sprout, the head of Hufflepuff House.</p>
+            <h3>Triwizard Tournament: Cedric's Finest Hour</h3>
+            <p>The Triwizard Tournament was an event that tested young witches and wizards beyond their limits. When the Goblet of Fire spit out Cedric's name as one of the champions, many were surprised, but those who knew him had faith in his capabilities. Cedric's participation in the Triwizard Tournament wasn't without its challenges. From retrieving a golden egg from a dragon to navigating a maze filled with dangerous creatures, he displayed immense courage and intellect.</p>
+            <br>
+            <div><em><strong>He successfully retrieved the egg, Rescued all hostages on time, and reached the Triwizard Cup with Harry. </strong></em></div>
+            <br>
+            <!--I didn't see any limitation on the size of the pictures and stuff, I definitly will change it later on for styling purpsoes.-->
+            <img src="images/ced.jpg" alt="ced's picture being chosen">
+        </article>
+    </main>
+    
+
+    <!-- FOOTER section -->
+    <footer>
+        <div>Copyright 2023, Bohan Cui</div>
+                <!-- CITATIONS -->
+        <section>
+            <h2>References</h2>
+            <ol>
+                <li><a href="https://en.wikipedia.org/wiki/Harry_Potter">Wikipedia Page</a></li>
+                <li><a href="https://harrypotter.fandom.com/wiki/Wand">Wand Sources</a></li>
+                <li><a href="https://harrypotter.fandom.com/wiki/Main_Page">Fandom Main Page</a></li>
+            </ol>
+        </section>
+    </footer>
+
+</body>
+
+</html>
